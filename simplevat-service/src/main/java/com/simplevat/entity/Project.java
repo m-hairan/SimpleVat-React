2d478fc5bf45fe179fbafced6e8c18d2f4cc70a1
UtkarshBhavsar <ubhavsar@mobiquityinc.com>
2017-05-20 12:20:19 +0530

    SV-55 : Project validation

--- simplevat-service/src/main/java/com/simplevat/entity/Project.java
+++ simplevat-service/src/main/java/com/simplevat/entity/Project.java
@@ -2,6 +2,7 @@ package com.simplevat.entity;
 
 import com.simplevat.entity.converter.DateConverter;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -14,25 +15,27 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "PROJECT")
 @Data
-public class Project implements Serializable{
+@NoArgsConstructor
+public class Project implements Serializable {
+
     @Id
     @Column(name = "PROJECT_ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer projectId;
-    @Basic
+
     @Column(name = "PROJECT_NAME")
     private String projectName;
-    @Basic
+
     @Column(name = "PROJECT_BUDGET")
     private BigDecimal projectBudget;
-    @Basic
+
     @Column(name = "CONTRACT_PO_NUMBER")
     private String contractPoNumber;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "CONTACT_ID")
     private Contact contact;
-    @Basic
+
     @Column(name = "VAT_REGISTRATION_NUMBER")
     private String vatRegistrationNumber;
 
@@ -44,23 +47,23 @@ public class Project implements Serializable{
     @JoinColumn(name = "CURRENCY_CODE")
     private Currency currency;
 
-    @Basic
+
     @Column(name = "CREATED_BY")
     private Integer createdBy;
-    @Basic
+
     @Column(name = "CREATED_DATE")
     @Convert(converter = DateConverter.class)
     private LocalDateTime createdDate;
-    @Basic
+
     @Column(name = "LAST_UPDATED_BY")
     private Integer lastUpdatedBy;
-    @Basic
+
     @Column(name = "LAST_UPDATE_DATE")
     private LocalDateTime lastUpdateDate;
-    @Basic
+
     @Column(name = "DELETE_FLAG")
     private Boolean deleteFlag = Boolean.FALSE;
-    @Basic
+
     @Version
     @Column(name = "VERSION_NUMBER")
     private Integer versionNumber = 1;
