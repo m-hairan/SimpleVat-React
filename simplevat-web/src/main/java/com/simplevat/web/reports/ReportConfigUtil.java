b05d7edf87d2c595eabc63d67fa59f7ddd908a26
Pankaj Tiwary <pankaj_tiwary@yahoo.com>
2017-09-07 10:49:59 +0530

    Excel Implementation of Jasper Report

--- simplevat-web/src/main/java/com/simplevat/web/reports/ReportConfigUtil.java
+++ simplevat-web/src/main/java/com/simplevat/web/reports/ReportConfigUtil.java
@@ -3,12 +3,22 @@ package com.simplevat.web.reports;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.URL;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.util.Map;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.simplevat.web.contact.controller.ContactController;
 
 import net.sf.jasperreports.engine.JRAbstractExporter;
 import net.sf.jasperreports.engine.JRDataSource;
@@ -17,13 +27,14 @@ import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.export.JRHtmlExporter;
 import net.sf.jasperreports.engine.export.JRHtmlExporterParameter;
 import net.sf.jasperreports.engine.export.JRXlsExporter;
 import net.sf.jasperreports.engine.export.JRXlsExporterParameter;
 
 public class ReportConfigUtil {
+	
+	private final static Logger LOGGER = LoggerFactory.getLogger(ReportConfigUtil.class);
 
 	public static String compileReport(String compileDir, String filename) {
 		try {
@@ -37,15 +48,15 @@ public class ReportConfigUtil {
 			System.out.println("jarsperFileAbsolutePath  " + jarsperFileAbsolutePath);
 			File tempFile = new File(jarsperFileAbsolutePath);
 			if (tempFile.exists()) {
-				System.out.println(jarsperFileAbsolutePath + " file Exists, Not compiling ");
+				LOGGER.info(jarsperFileAbsolutePath + " file Exists, Not compiling ");
 			} else {
-				System.out.println(jarsperFileAbsolutePath + " file does not Exists, compiling ");
+				LOGGER.info(jarsperFileAbsolutePath + " file does not Exists, compiling ");
 				JasperCompileManager.compileReportToFile(file.getAbsolutePath(), jarsperFileAbsolutePath);
 			}
 
 			return jarsperFileAbsolutePath;
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			LOGGER.error("Exception while compiling the JasperReport " + e.getMessage());
 			e.printStackTrace();
 			return null;
 		}
@@ -105,11 +116,11 @@ public class ReportConfigUtil {
 		exportReport(exporter, jasperPrint, out);
 	}
 
-	public static void exportReportAsExcel(JasperPrint jasperPrint, PrintWriter out)
+	public static void exportReportAsExcelOld(JasperPrint jasperPrint, PrintWriter out)
 			throws JRException, FileNotFoundException, IOException {
 		ByteArrayOutputStream output = new ByteArrayOutputStream();
-		// OutputStream outputfile = new FileOutputStream(new
-		// File("d:/output/JasperReport1.xls"));//make sure to have the directory. excel
+		 OutputStream outputfile = new FileOutputStream(new
+		 File("d:/dev/JasperReport1.xls"));//make sure to have the directory. excel
 		// file will export here
 
 		// coding For Excel:
@@ -120,10 +131,29 @@ public class ReportConfigUtil {
 		exporterXLS.setParameter(JRXlsExporterParameter.IS_DETECT_CELL_TYPE, Boolean.TRUE);
 		exporterXLS.setParameter(JRXlsExporterParameter.IS_WHITE_PAGE_BACKGROUND, Boolean.FALSE);
 		exporterXLS.setParameter(JRXlsExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_ROWS, Boolean.TRUE);
-		// exporterXLS.exportReport();
-		// outputfile.write(output.toByteArray());
-		exportReport(exporterXLS, jasperPrint, out);
+		exporterXLS.exportReport();
+		outputfile.write(output.toByteArray());
+		//exportReport(exporterXLS, jasperPrint, out);
 	}
 
+	public static void exportReportAsExcel(JasperPrint jasperPrint, HttpServletResponse response)
+			throws JRException, FileNotFoundException, IOException {
+		ServletOutputStream out = response.getOutputStream();
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		JRXlsExporter exporterXLS = new JRXlsExporter();
+		exporterXLS.setParameter(JRXlsExporterParameter.JASPER_PRINT, jasperPrint);
+		exporterXLS.setParameter(JRXlsExporterParameter.OUTPUT_STREAM, output);
+		//exporterXLS.setParameter(JRExporterParameter.OUTPUT_WRITER, out);
+		exporterXLS.setParameter(JRXlsExporterParameter.IS_ONE_PAGE_PER_SHEET, Boolean.FALSE);
+		exporterXLS.setParameter(JRXlsExporterParameter.IS_DETECT_CELL_TYPE, Boolean.TRUE);
+		exporterXLS.setParameter(JRXlsExporterParameter.IS_WHITE_PAGE_BACKGROUND, Boolean.FALSE);
+		exporterXLS.setParameter(JRXlsExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_ROWS, Boolean.TRUE);
+		exporterXLS.exportReport();
+		response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+		response.setHeader("Content-Disposition", "inline; filename=SimpleVatReport.xls");
+		out.write(output.toByteArray());
+		out.flush();
+		out.close();
+	}
 
 }
