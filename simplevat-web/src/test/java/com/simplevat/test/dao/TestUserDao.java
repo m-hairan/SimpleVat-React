2bf6815b2a68a00e0097b93167d56431257e9825
udaykumarmca <udaykumar.mca@gmail.com>
2017-10-06 18:59:41 +0530

    code commented

--- simplevat-web/src/test/java/com/simplevat/test/dao/TestUserDao.java
+++ simplevat-web/src/test/java/com/simplevat/test/dao/TestUserDao.java
@@ -17,19 +17,19 @@ public class TestUserDao extends BaseManagerTest {
     @Test
     @Ignore
     public void testGetRoleById() {
-        User user = new User();
-        user.setCreatedBy(1);
-        user.setCompanyId(1);
-        user.setCreatedDate(LocalDateTime.now());
-        user.setDateOfBirth(LocalDateTime.now());
-        user.setUserEmail("shethhiren93@gmail.com");
-        user.setFirstName("Hiren");
-        user.setLastName("Sheth");
-        user.setLastUpdateDate(LocalDateTime.now());
-        user.setRoleCode(1);
-        user.setUserId(1);
-        user.setVersionNumber(1);
-        userDao.persist(user);
+//		  User user = new User();
+//        user.setCreatedBy(1);
+//        user.setCompanyId(1);
+//        user.setCreatedDate(LocalDateTime.now());
+//        user.setDateOfBirth(LocalDateTime.now());
+//        user.setUserEmail("shethhiren93@gmail.com");
+//        user.setFirstName("Hiren");
+//        user.setLastName("Sheth");
+//        user.setLastUpdateDate(LocalDateTime.now());
+//        user.setRoleCode(1);
+//        user.setUserId(1);
+//        user.setVersionNumber(1);
+//        userDao.persist(user);
         
     }
 }
