557782313dad28c7643c99a45c4e0eaeca31b5f8
udaykumarmca <udaykumar.mca@gmail.com>
2017-10-16 11:08:26 +0530

    File Name Modified

---
+++ simplevat-web/src/main/java/com/simplevat/web/converter/UserConverter.java
@@ -0,0 +1,42 @@
+package com.simplevat.web.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import com.simplevat.entity.User;
+import com.simplevat.service.UserServiceNew;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author bhavin.panchani
+ *
+ */
+@Service
+public class UserConverter implements Converter {
+
+    @Autowired
+    private UserServiceNew userServiceNew;
+    
+	@Override
+	public Object getAsObject(FacesContext fc, UIComponent uic, String string) {
+		if (string != null && !string.isEmpty()) {
+			User user = userServiceNew.findByPK(Integer.parseInt(string));
+			return user;
+		}
+		return null;
+	}
+
+	@Override
+	public String getAsString(FacesContext fc, UIComponent uic, Object o) {
+		
+		if (o instanceof User) {
+			User user = (User) o;
+			return user.getUserId().toString();
+		}
+		return null;
+	}
+
+}
