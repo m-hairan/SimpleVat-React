a2d774eaa998537c5304dfd6df4e3f1e87180947
Mohsin Hashmi <myselfmohsin@gmail.com>
2017-12-12 16:53:08 +0400

    selector app

--- rolling-update-image.bash
+++ rolling-update-image.bash
@@ -6,5 +6,9 @@ kubectl get replicationcontroller -o=yaml > replicationcontroller.yaml
 sed -i "/^  metadata:/,/^  [a-z]/!b;/\(^    name:\|^    selfLink:\)/!b;s/$/-XXXXXXXX/" replicationcontroller.yaml
 
 
-# Replace app
+# Replace metadata app
 sed -i "/^  metadata:/,/^  [a-z]/!b;/^    labels:/,/^    [a-z]/!b;/^      app:/!b;s/$/-XXXXXXXX/" replicationcontroller.yaml
+
+
+# Replace selector app
+sed -i "/^  spec:/,/^  [a-z]/!b;/^    selector:/,/^    [a-z]/!b;/^      app:/!b;s/$/-XXXXXXXX/" replicationcontroller.yaml
