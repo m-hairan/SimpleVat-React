305551ba8dfc6a371963e80a8ab206a0ec41d39c
Mohsin Hashmi <myselfmohsin@gmail.com>
2017-12-12 16:51:01 +0400

    replace app

--- rolling-update-image.bash
+++ rolling-update-image.bash
@@ -4,3 +4,7 @@ kubectl get replicationcontroller -o=yaml > replicationcontroller.yaml
 
 # Replace name and selfLink 
 sed -i "/^  metadata:/,/^  [a-z]/!b;/\(^    name:\|^    selfLink:\)/!b;s/$/-XXXXXXXX/" replicationcontroller.yaml
+
+
+# Replace app
+sed -i "/^  metadata:/,/^  [a-z]/!b;/^    labels:/,/^    [a-z]/!b;/^      app:/!b;s/$/-XXXXXXXX/" replicationcontroller.yaml
