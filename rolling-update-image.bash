237530abf1ed11fe13cc0369a6ff872b8f0d3ef9
Mohsin Hashmi <myselfmohsin@gmail.com>
2017-12-12 16:43:38 +0400

    just name

--- rolling-update-image.bash
+++ rolling-update-image.bash
@@ -4,18 +4,3 @@ kubectl get replicationcontroller -o=yaml > replicationcontroller.yaml
 
 # Replace name and selfLink 
 sed -i "/^  metadata:/,/^  [a-z]/!b;/\(^    name:\|^    selfLink: \/api/v1\/namespaces\/simplevat-dev\/replicationcontrollers\)/!b;s/$/-XXXXXXXX/" replicationcontroller.yaml
-
-# Replace app
-sed -i "/^  metadata:/,/^  [a-z]/!b;/^    labels:/,/^    [a-z]/!b;/^      app:/!b;s/$/-XXXXXXXX/" replicationcontroller.yaml
-
-# Replace app
-sed -i "/^spec:/,/^[a-z]/!b;/^  selector:/,/^  [a-z]/!b;/^    app:/!b;s/$/-XXXXXXXX/" replicationcontroller.yaml
-
-# Replace app
-sed -i "/^spec:/,/^[a-z]/!b;/^  template:/,/^  [a-z]/!b;/^    metadata:/,/^    [a-z]/!b;/^      labels:/,/^      [a-z]/!b;/^        app:/!b;s/$/-XXXXXXXX/" replicationcontroller.yaml
-
-#Update Image
-sed -i "s/        image: gcr.io\/simplevat-cluster\/simplevat/        image: gcr.io\/simplevat-cluster\/simplevat:XYZ/g" replicationcontroller.yaml
-
-# Replace release  
-sed -i "/^      containers:/,/^      [a-z]/!b;/^      - env:/,/^        [a-z]/!b;/^        - name: SIMPLEVAT_RELEASE/!b;n;s/^          value:.*/          value: XXXXXXXX/" replicationcontroller.yaml
