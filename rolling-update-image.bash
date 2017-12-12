05909fe895e5b7610970f6a986c9ff8a9ab42d9e
Mohsin Hashmi <myselfmohsin@gmail.com>
2017-12-12 16:49:35 +0400

    test

--- rolling-update-image.bash
+++ rolling-update-image.bash
@@ -3,4 +3,4 @@
 kubectl get replicationcontroller -o=yaml > replicationcontroller.yaml
 
 # Replace name and selfLink 
-sed -i "/^  metadata:/,/^  [a-z]/!b;/\(^    name:\|^    selfLink: \/api/v1\/namespaces\/simplevat-dev\/replicationcontrollers\)/!b;s/$/-XXXXXXXX/" replicationcontroller.yaml
+sed -i "/^  metadata:/,/^  [a-z]/!b;/\(^    name:\|^    selfLink:\)/!b;s/$/-XXXXXXXX/" replicationcontroller.yaml
