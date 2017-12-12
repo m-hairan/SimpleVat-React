889a2471bd18a7d0d2fd778fd124caa78f4d730b
Mohsin Hashmi <myselfmohsin@gmail.com>
2017-12-12 16:42:31 +0400

    adjust spacing

--- rolling-update-image.bash
+++ rolling-update-image.bash
@@ -3,10 +3,10 @@
 kubectl get replicationcontroller -o=yaml > replicationcontroller.yaml
 
 # Replace name and selfLink 
-sed -i "/^metadata:/,/^[a-z]/!b;/\(^  name:\|^  selfLink:\)/!b;s/$/-XXXXXXXX/" replicationcontroller.yaml
+sed -i "/^  metadata:/,/^  [a-z]/!b;/\(^    name:\|^    selfLink: \/api/v1\/namespaces\/simplevat-dev\/replicationcontrollers\)/!b;s/$/-XXXXXXXX/" replicationcontroller.yaml
 
 # Replace app
-sed -i "/^metadata:/,/^[a-z]/!b;/^  labels:/,/^  [a-z]/!b;/^    app:/!b;s/$/-XXXXXXXX/" replicationcontroller.yaml
+sed -i "/^  metadata:/,/^  [a-z]/!b;/^    labels:/,/^    [a-z]/!b;/^      app:/!b;s/$/-XXXXXXXX/" replicationcontroller.yaml
 
 # Replace app
 sed -i "/^spec:/,/^[a-z]/!b;/^  selector:/,/^  [a-z]/!b;/^    app:/!b;s/$/-XXXXXXXX/" replicationcontroller.yaml
