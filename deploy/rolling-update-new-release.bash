641260dbe9b5e17f52f8e7e392d14e7e20e9f11a
Mohsin Hashmi <myselfmohsin@gmail.com>
2017-12-13 08:42:41 +0400

    added some echo comments

--- deploy/rolling-update-new-release.bash
+++ deploy/rolling-update-new-release.bash
@@ -1,23 +1,28 @@
 #!/bin/bash
-
-kubectl get replicationcontroller > rc.txt
+echo " Executing kubectl get replicationcontrollers --namespace=simplevat-dev > rc.txt"
+kubectl get replicationcontrollers --namespace=simplevat-dev > rc.txt
 
 i=0
+echo "Upgrading replication controllers" 
 while read line 
 do
   if [ $i != 0 ]
   then
     rc=($line)
     domain=${rc:0:-4}
-    echo "$domain"
+    echo "Upgrading Replication Controller for $domain"
     
     if [ $domain == "alpha" ]
     then
 
       ./simplevat-new-release-script.bash "$domain" dev "$1"
+      
+      echo "Replication Controller for $domain upgraded"
 
     fi
 
   fi
   i=$((i+1))
 done < rc.txt
+
+echo "All Replication Controller Successfully."
