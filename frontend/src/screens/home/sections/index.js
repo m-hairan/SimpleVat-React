17c1b64fb341a212be82a39f7fe7511c75787f73
unitech1120 <uni.tech1120@gmail.com>
2019-10-18 16:33:51 +0700

    Dashboard UI

---
+++ frontend/src/screens/home/sections/index.js
@@ -0,0 +1,13 @@
+import Invoice from './invoice'
+import BankAccount from './bank_account'
+import CashFlow from './cash_flow'
+import RevenueAndExpense from './revenue_expense'
+import ProfitAndLoss from './profit_loss'
+
+export {
+	Invoice,
+  BankAccount,
+	CashFlow,
+	RevenueAndExpense,
+	ProfitAndLoss
+}
