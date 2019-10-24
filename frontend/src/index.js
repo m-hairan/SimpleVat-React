704bc13881b6eceb9a5068b9733c7d7cd1a6f0ba
it-devros <it-devros@outlook.com>
2019-10-24 16:14:18 -0400

    table theme updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import 'polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import 'assets/css/global.css'
+import 'assets/css/global.scss'
 
 import App from 'app'
 import * as serviceWorker from 'serviceWorker'
