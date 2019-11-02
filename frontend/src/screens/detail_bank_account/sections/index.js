d8f71f3d43366e93da0c061609aee73c1cc9781d
it-devros <it-devros@outlook.com>
2019-11-02 07:59:36 -0400

    structure updated

--- frontend/src/screens/detail_bank_account/sections/index.js
+++ frontend/src/screens/detail_bank_account/sections/index.js
@@ -1,9 +1,11 @@
+import Temp from './temp'
 import BankStatementList from './bank_statement_list'
 import UpdateBankAccount from './update_bank_account'
 import CreateBankStatement from './create_bank_statement'
 import ImportBankStatementsCSV from './import_bank_statements_csv'
 
 export {
+  Temp,
   BankStatementList,
   UpdateBankAccount,
   CreateBankStatement,
