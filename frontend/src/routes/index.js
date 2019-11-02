77a6811b7607b56c26dea74ce464464febc28876
it-devros <it-devros@outlook.com>
2019-11-02 08:03:21 -0400

    health page removed

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,11 +1,9 @@
 import mainRoutes from './main'
 import initialRoutes from './initial'
 import adminRoutes from './admin'
-import healthRoutes from './health'
 
 export {
   mainRoutes,
   initialRoutes,
-  adminRoutes,
-  healthRoutes
+  adminRoutes
 }
