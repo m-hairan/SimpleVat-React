af2c0c54784082ec0bf1bcc3b288d9f498291ec9
it-devros <it-devros@outlook.com>
2019-11-06 07:16:52 -0500

    purchase updated

--- frontend/src/screens/purchase/reducer.js
+++ frontend/src/screens/purchase/reducer.js
@@ -1,12 +1,19 @@
 import { PURCHASE } from 'constants/types'
 
 const initState = {
+  purchase_list: []
 }
 
 const PurchaseReducer = (state = initState, action) => {
   const { type, payload} = action
   
   switch(type) {
+
+    case PURCHASE.PURCHASE_LIST:
+      return {
+        ...state,
+        purchase_list: Object.assign([], payload.data)
+      }
 
     default:
       return state
