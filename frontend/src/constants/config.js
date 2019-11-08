79d967952df646ae66040e85e10e0693569e6aa9
it-devros <it-devros@outlook.com>
2019-11-08 16:14:00 -0500

    API base url updated

--- frontend/src/constants/config.js
+++ frontend/src/constants/config.js
@@ -1,5 +1,4 @@
 export default {
-  API_ROOT_URL: 'http://localhost/'
   // API_ROOT_URL: 'http://k8s.dev.simplevat.com/'
-  // API_ROOT_URL: 'https://simplevat-dev.appspot.com/'
+  API_ROOT_URL: 'https://simplevat-dev.appspot.com/'
 }
