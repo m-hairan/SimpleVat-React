40dfca83da19a03164b4891fb0e15e865f618f84
sonu.lohar <sonu.lohar@daynilgroup.com>
2019-11-27 17:51:11 +0530

    package name updated

--- src/main/java/com/simplevat/rest/transactioncontroller/TransactionModel.java
+++ src/main/java/com/simplevat/rest/transactioncontroller/TransactionModel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.simplevat.rest.transactionscontroller;
+package com.simplevat.rest.transactioncontroller;
 
 import com.simplevat.entity.bankaccount.BankAccount;
 import com.simplevat.entity.bankaccount.TransactionCategory;
